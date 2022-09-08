8ab7cd7512a17688245b1db2d0aa87fe4d81c346
kuryart <kuryart1@gmail.com>
2022-09-08 16:43:44 -0300

    feat: instala Vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
