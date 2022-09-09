f7fba22cc6fb0d0dbfd4657463b55a7e54bd7ce8
kuryart <kuryart1@gmail.com>
2022-09-09 16:50:20 -0300

    feat: cria menu de navegação superior

---
+++ src/components/NavbarTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <v-toolbar dark prominent>
+        <v-img :src="logo" contain height="120" />
+      </v-toolbar>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from "../assets/img/grupo-a-logo.svg";
+
+export default {
+  name: "NavbarTop",
+
+  data: () => ({
+    logo,
+  }),
+};
+</script>
