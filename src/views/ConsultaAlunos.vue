cb4f8102f90ff3473ba828c91cf9e1cd1a30f900
kuryart <kuryart1@gmail.com>
2022-09-09 16:49:23 -0300

    feat: cria view para consultar alunos

---
+++ src/views/ConsultaAlunos.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-banner class="pt-2 pb-2">Consulta de Alunos</v-banner>
+  <search-bar></search-bar>
+  <alunos-data-table></alunos-data-table>
+</template>
+
+<script>
+import SearchBar from "../components/SearchBar.vue";
+import AlunosDataTable from "../components/AlunosDataTable.vue";
+
+export default {
+  name: "ConsultaAlunos",
+
+  components: {
+    SearchBar,
+    AlunosDataTable,
+  },
+};
+</script>
