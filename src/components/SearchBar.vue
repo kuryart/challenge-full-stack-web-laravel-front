cc6dfaa025e3be5324de36ea6a43d0ec0e254d95
kuryart <kuryart1@gmail.com>
2022-09-09 16:50:44 -0300

    feat: cria barra de pesquisa

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex mt-1 align-center">
+    <v-text-field label="Digite sua busca" class="ml-10 pt-5"></v-text-field>
+    <v-btn elevation="1">Pesquisar</v-btn>
+    <v-btn class="ml-10 mr-10">Cadastrar Aluno</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<style>
+</style>
